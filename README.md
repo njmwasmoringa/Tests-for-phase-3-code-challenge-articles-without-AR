6f2bd4d62aa98148d251c876048e45313f195152
mwangi julius <julius.mwangi@moringaschool.com>
2022-11-01 05:12:55 +0300

    Phase 3 code challenge test script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Tests-for-phase-3-code-challenge-articles-without-AR
+# phase 3 Code Challenge: Articles without AR Test Script
+app_spec.rb contains all the tests for testing the code for Phase 3 code challenge
+
+## How to run the test
+- `git clone` this repository
+- then run `bash tester.sh <repo-link>` replace "repo-link" with a repository link to the code challenge
+
+this will
+1. clone the repository
+2. install all the ruby gems using bundle install
+3. setup the tests
+4. run the test
---
+++ tester.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Read command arguments and extract repolink, reponame and username
+repolink=$1
+readarray -d / -t strarr <<<"$repolink"
+arraylen=`expr ${#strarr[*]} - 1`
+usernameIndex=`expr ${arraylen} - 1`
+reponame=${strarr[arraylen]}
+username=${strarr[usernameIndex]}
+readarray -d . -t repoparts <<<"$reponame"
+reponame=${repoparts[0]}
+echo "Welcome to lab automation"
+echo "Running Tests, please wait..."
+
+workingDir=$(pwd)
+mkdir $username
+cd $username
+
+# handle when repository already exists
+if [ -d $reponame ]; then
+    rm -r -f $reponame
+fi
+
+git clone $repolink
+#continue after checking repo existance
+
+# Handle any cloning failure by retrying 5 times
+if [ ! -d $reponame ]; then
+    tries=0
+    while [ $tries -lt 5 ]
+    do
+        git clone $repolink
+        if [ ! -d $reponame ]; then
+            tries=`expr $tries + 1`
+            if [ $tries -eq 5]; then
+                echo "Failed to clone the repository"
+                exit
+            fi
+        fi
+    done
+fi
+# Stop handling the clone failures
+echo "Cloned"
+
+cd $reponame
+
+# git remote add origin1 $repolink
+# git remote remove origin
+# git remote add origin "$workingDir/$username/$username.bundle"
+# git remote -v
+
+mkdir "spec"
+cp ../../app_spec.rb spec/spec_app.rb
+
+rspec --init
+
+# sleep 1
+
+# echo "Installing ruby gems"
+# bundle install
+
+# sleep 1
+
+rm Gemfile.lock .rspec spec/spec_helper.rb
+sleep 1
+rspec --init
+sleep 1
+bundle install
+sleep 1
+
+#perform the tests
+# echo "Running ruby tests"
+rspec spec/spec_app.rb
+
+#cleaning up
+cd $workingDir
+# rm -r -f $username
